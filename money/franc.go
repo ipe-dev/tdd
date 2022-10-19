9852a678a33b3c172ecfbee68dd85a839bd14822
ipe <izaki.yb1998@gmail.com>
2022-10-19 21:36:51 +0900

    9章

--- money/franc.go
+++ money/franc.go
@@ -3,5 +3,3 @@ package money
 type Franc struct {
 	Money 
 }
-
-
--- money/money.go
+++ money/money.go
@@ -1,22 +1,37 @@
 package money
 
-type MoneyInterface interface {
-	Times(int) Money
+type Accessor interface {
+	Amount() int
+	Currency() string
 }
 
 type Money struct {
 	amount int
-	name string
+	currency string
+}
+
+func NewMoney(amount int, currency string) Money {
+	return Money{
+		amount: amount,
+		currency: currency,
+	}
+}
+func (m Money) Amount() int {
+	return m.amount
+}
+
+func (m Money) Currency() string {
+	return m.currency
 }
 
 func (m Money) Equals(money Money) bool {
-	return m.amount == money.amount && m.name == money.name
+	return m.amount == money.amount && m.currency == money.currency
 }
 
 func NewDollar(amount int) Money {
 	return Money{
 		amount: amount,
-		name: "dollar",
+		currency: "dollar",
 	}
 }
 
@@ -24,12 +39,12 @@ func (m Money) Times(multiplier int) Money {
 	// これがコピーを返すっていう意味か！
 	return Money{
 		amount: m.amount * multiplier,
-		name: m.name,
+		currency: m.currency,
 	}
 }
 func NewFranc(amount int) Money {
 	return Money {
 		amount: amount,
-		name: "franc",
+		currency: "franc",
 	}
 }
